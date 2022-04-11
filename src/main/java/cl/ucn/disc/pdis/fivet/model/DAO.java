8ae2563d16ad65002506fd1222de6033e232f293
Diego Urrutia-Astorga <mika.cl@gmail.com>
2022-04-11 15:11:34 -0400

    feat(ORM): add the DAO interface

---
+++ src/main/java/cl/ucn/disc/pdis/fivet/model/DAO.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2022 Diego Urrutia-Astorga.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ */
+
+package cl.ucn.disc.pdis.fivet.model;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * The DAO Interface.
+ *
+ * @param <T>
+ * @author Diego Urrutia-Astorga.
+ */
+public interface DAO<T extends BaseEntity> {
+
+    /**
+     * Get optional T.
+     *
+     * @param id to retrieve.
+     */
+    Optional<T> get(Integer id);
+
+    /**
+     * Get all the T.
+     */
+    List<T> getAll();
+
+    /**
+     * Save T into the  T.
+     *
+     * @param t to save.
+     */
+    void save(T t);
+
+    /**
+     * Delete a T.
+     *
+     * @param t to delete.
+     */
+    void delete(T t);
+
+    /**
+     * Delete a T.
+     *
+     * @param id to delete.
+     */
+    void delete(Integer id);
+
+}
